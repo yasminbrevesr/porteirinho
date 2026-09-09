68dd228091fc006f99074ee12b54b2ada39f5e74
b <brxvesyasmin@gmail.com>
2026-09-09 17:17:20 -0300

    fix: usa opt-in AndroidX no scanner

--- app/src/main/java/br/com/porteirinho/ui/QrScannerView.kt
+++ app/src/main/java/br/com/porteirinho/ui/QrScannerView.kt
@@ -73,7 +73,7 @@ fun QrScannerView(onScanned: (String) -> Unit, onCancel: () -> Unit) {
 }
 
 @Composable
-@ExperimentalGetImage
+@androidx.annotation.OptIn(ExperimentalGetImage::class)
 private fun CameraPreview(onScanned: (String) -> Unit, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
